09f25f36886099f887835919a3acb3770837bfbe
Gerardo Villalba <gmartinvm9@gmail.com>
2026-03-11 17:21:32 +0100

    se agrego el challenge de jose

---
+++ trivia.py
@@ -0,0 +1,9 @@
+puntos = 0
+
+usuario = input('ingrese su nombre')
+print(f'bienvenido al juego', {usuario})
+
+primera_pregunta = input('cual es la capital de paraguay? a: capiata b: carapegua c: asuncion')
+if (primera_pregunta == 'c' or primera_pregunta == 'c'):
+    puntos += 1 
+
